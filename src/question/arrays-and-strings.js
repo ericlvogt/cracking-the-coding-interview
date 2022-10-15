a8e69eece6fe43cb2e93b8758bbf101b592fad17
Eric Vogt <eric.lvogt@gmail.com>
2022-10-14 22:46:15 -0600

    Renamed question1_1 method and added a new test

--- src/question/arrays-and-strings.js
+++ src/question/arrays-and-strings.js
@@ -3,13 +3,14 @@
 /** @class Chapter 1 Arrays and Strings. */
 export class ArraysAndStrings {
   /**
+   * 1.1
    * Implement an algorithm to determine if a string has all unique
    * characters. What if you cannot use additional data structures?
    * @method
    * @param {string} value
    * @returns
    */
-  question1_1(value) {
+  allUniqueCharacters(value) {
     return value[0];
   };
 }
--- src/test/arrays-and-strings.test.js
+++ src/test/arrays-and-strings.test.js
@@ -7,10 +7,14 @@ const arraysAndStrings = new ArraysAndStrings();
 
 describe('ArraysAndStrings', () =>
   it('question 1.1 should handle empty string', () =>{
-    const result = arraysAndStrings.question1_1('1');
+    const result = arraysAndStrings.allUniqueCharacters('1');
     assert.strictEqual(result, null);
   }),
 it('question 1.1 should handle invalid entry', () =>{
   assert.strictEqual(1, 1);
+}),
+it('question 1.1 should detect duplicate char', () =>{
+  const result = arraysAndStrings.allUniqueCharacters('11');
+  assert.strictEqual(result, false);
 }),
 );
