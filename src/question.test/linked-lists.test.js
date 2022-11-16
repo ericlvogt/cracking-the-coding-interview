9b4102956772eb89407fc8b409db59a68fa49cc2
Eric Vogt <eric.lvogt@gmail.com>
2022-11-15 21:49:56 -0700

    added settings for Mocha Test Explorer

--- src/question.test/linked-lists.test.js
+++ src/question.test/linked-lists.test.js
@@ -10,19 +10,41 @@ describe('LinkedLists', function() {
     describe('#removeDuplicates', function() {
       it('should return same list when there are no duplicates', function() {
         const values = [1, 2, 3, 4, 5];
+
         let linkedList = new LinkedList();
-        
         for(let value in values){
           linkedList.appendToEnd(value);
         }
 
-        const result = linkedLists.removeDuplicates();
+        const actual = linkedLists.removeDuplicates(linkedList);
         
-        assert.strictEqual(result.length, values.length);
+        assert.strictEqual(actual.length, values.length);
+
+        let node = linkedList;
+        for(let value in values){
+          assert.strictEqual(node.value, value)
+          node = node.next;
+        }
 
       });
       it('should return list with a removed duplicate', function() {
-        assert.fail();
+        const values = [1, 3, 2, 2, 4, 5, 3, 2];
+        const expected = [1, 3, 2, 4, 5];
+
+        let linkedList = new LinkedList();
+        for(let value in values){
+          linkedList.appendToEnd(value);
+        }
+
+        const actual = linkedLists.removeDuplicates(linkedList);
+
+        assert.strictEqual(actual.length, expected.length);
+
+        let node = linkedList;
+        for(let value in expected){
+          assert.strictEqual(node.value, value)
+          node = node.next;
+        }
       });
     });
 });
